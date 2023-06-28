baf800fc7fda69bdefcb3081a4f6993b381becad
JungMY <jmy931004@gmail.com>
2023-06-28 22:35:28 +0900

    임시로 만들어본 / 모집 역할로 목록을 뽑아오는 거에 대한 getmapping 구현 DB쪽이 확실하게 정해지고 DTO등 파일 만들어지면 구현 예정

--- src/main/java/com/recruit/kr/controller/BoardController.java
+++ src/main/java/com/recruit/kr/controller/BoardController.java
@@ -19,10 +19,25 @@ public class BoardController {
     //여기에 쓰인 이름은 가칭이므로 추후 변동 가능
     @GetMapping("/api/board/{boardWantedRole}")
     public List<Board> getBoard(@PathVariable String boardWantedRole){
-        if(boardWantedRole.equals("프론트엔드")){
-            System.out.println("test");
+        if(boardWantedRole.equals("all")){
+            return boardJparepo.findAllByOrderByBoardIdDesc();
         }
-        return boardJparepo.findAllByOrderByBoardIdDesc();
+        else if(boardWantedRole.equals("프론트엔드")){
+            return boardJparepo.findAllByBoardWantedRoleContainingOrderByBoardIdDesc(boardWantedRole);
+        }
+        else if(boardWantedRole.equals("백엔드")){
+            return boardJparepo.findAllByBoardWantedRoleContainingOrderByBoardIdDesc(boardWantedRole);
+        }
+        else if(boardWantedRole.equals(("보안"))){
+            return boardJparepo.findAllByBoardWantedRoleContainingOrderByBoardIdDesc(boardWantedRole);
+        }
+        else if(boardWantedRole.equals("AI")){
+            return boardJparepo.findAllByBoardWantedRoleContainingOrderByBoardIdDesc(boardWantedRole);
+        }
+        //위에 코드 압축 가능할지도? 어차피 우리가 원하는 검색 요소가 4가지로 카테고리 있는거니깐
+        else{
+            return null;
+        }
     }
     //등록 API
     @PostMapping("/api/board")
@@ -31,22 +46,12 @@ public class BoardController {
         boardJparepo.save(board);
         return board;
     }
-    /* ID로 검색하는 영역인데 삭제해도 무관
-    @PutMapping("/api/board/{boardid}")
-    public Board putBoard(@PathVariable Long boardid, BoardDTO boardDTO){
-        Optional<Board> optionalBoard = boardJparepo.findById(boardid);
-        Board board = new Board();
-        if(optionalBoard.isPresent()){
-            board = optionalBoard.get();
 
-        }
-        return board;
-
-    }*/
+    /*
     //제목으로 검색하기
     //Board_JPAREPO를 통해서 원하는 대로 수정하면 될듯
     @PutMapping("/api/board/{boardTitle}")
-    public Board putBoardTitle(@PathVariable String boardTitle, BoardDTO boardDTO){
+    public Board putBoardTitle(@PathVariable String boardTitle){
         Optional<Board> optionalBoard = boardJparepo.findByBoardTitle(boardTitle);
 
         Board board = new Board();
@@ -57,15 +62,20 @@ public class BoardController {
 
         return board;
     }
-    @PutMapping("/api/board/sub/{boardTitle}")
+    */
+    @PutMapping("/api/board/{boardTitle}")
     public List<Board> putBoardTitleSub(@PathVariable String boardTitle){
-        return  boardJparepo.findBoardByBoardTitleContainingOrderByBoardIdDesc(boardTitle);
+        List<Board> boardList = boardJparepo.findBoardByBoardTitleContainingOrderByBoardIdDesc(boardTitle);
+        if(boardList.size()!=0){
+            return boardList;
+        }
+        else{ return null;}
 
     }
 
     //삭제 API
     @DeleteMapping("/api/board/{boardId}")
-    public void deleteBoard(@PathVariable Long boardId, BoardDTO boardDTO){
+    public void deleteBoard(@PathVariable Long boardId){
         boardJparepo.deleteById(boardId);
     }
 
@@ -73,7 +83,7 @@ public class BoardController {
     @PatchMapping("/api/board/{boardId}")
     public void updateBoard(@PathVariable Long boardId, @RequestBody BoardDTO boardDTO){
         Optional<Board> optionalBoard = boardJparepo.findById(boardId);
-        Board board = new Board();
+
         if(optionalBoard.isPresent()){
             boardService.updateBoard(boardId,boardDTO);
         }
--- src/main/java/com/recruit/kr/domain/Board_JPAREPO.java
+++ src/main/java/com/recruit/kr/domain/Board_JPAREPO.java
@@ -12,12 +12,17 @@ import java.util.Optional;
 @EnableJpaAuditing
 public interface Board_JPAREPO extends JpaRepository<Board,Long> {
 
+    //모든 모집 역할에 대해서 받아올 수 있게 하는 것
     List<Board> findAllByOrderByBoardIdDesc();
 
-    // get list with searching bt title(can sub string)
+    //특정한 모집 역할에 대해 받아오는 리스트
+    List<Board> findAllByBoardWantedRoleContainingOrderByBoardIdDesc(String boardWantedRole);
+
+    // 검색시 사용하는 리스트
     List<Board> findBoardByBoardTitleContainingOrderByBoardIdDesc(String title);
 
-    Optional<Board> findByBoardTitle(String boardTitle);
+    //수정할때 사용하는 Optional
+   // Optional<Board> findByBoardTitle(String boardTitle);
 
 
 }
