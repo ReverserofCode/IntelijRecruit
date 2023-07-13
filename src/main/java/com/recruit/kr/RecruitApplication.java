45eab8d1c87c3cc82996e88442b6d85ad2123466
ReverserofCode <patteon@naver.com>
2023-07-13 11:34:52 +0900

    0713 다른 브렌치와 병합 완료
    게시판 작성 수정 삭제가 로그인 한 사용자와 작성자가 같아야 작동함
    게시판 자동 리다이렉트 및 리프레시 기능 추가
    
    회원 탈퇴 추가

--- src/main/java/com/recruit/kr/RecruitApplication.java
+++ src/main/java/com/recruit/kr/RecruitApplication.java
@@ -7,9 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class RecruitApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RecruitApplication.class, args);
 	}
-
 }
